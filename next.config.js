a3f5129b8b7d55b7a658b8c5efded678a1432622
Liquid2112 <Shubhpatel2504@gmail.com>
2026-09-23 00:35:17 -0500

    Add basePath and assetPrefix for GitHub Pages subdirectory

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/clearflow-mvp',
+  assetPrefix: '/clearflow-mvp',
   experimental: {
     optimizePackageImports: ['@/components'],
   },
